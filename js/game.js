5a4cb03dd1109cf3ed42600a90983d0011a9df67
Lelio <lelio.rollin@e-artsup.net>
2021-01-20 18:37:14 +0100

    Gyroscope test

--- js/game.js
+++ js/game.js
@@ -22,6 +22,7 @@ const objs = [];
 const colors = ['#ffba08','#faa307','#f48c06','#e85d04','#dc2f02','#d00000','#9d0208','#6a040f','#370617','#03071e']
 
 let gyroscope = new Gyroscope({frequency: 60});
+gyroscope.start();
 
 
 function getRandomInt(max) {
