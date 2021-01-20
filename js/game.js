a3b86901f5694a9b749c13fa7e8fbb218518b36a
Lelio <lelio.rollin@e-artsup.net>
2021-01-20 18:24:50 +0100

    Circles

--- js/game.js
+++ js/game.js
@@ -1,45 +1,85 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-let x = 20
-let y = 20
-
-let rectWidth = 100
-let rectHeight = 100
-
-let goUp = new Boolean ("false");
-let goRight = new Boolean("true")
+canvas.width = document.documentElement.clientWidth || document.body.clientWidth;
+canvas.height = document.documentElement.clientHeight || document.body.clientHeight;
 
 
-function gameloop(){
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    
-    ctx.fillStyle = 'green';
-    ctx.fillRect(x, y, rectWidth, rectHeight);
-    
-    if (goUp) {
-        y += 1
-    }
-    else{
-        y -= 1
-    }
 
-    if (goRight) {
-        x += 1
-    }
-    else{
-        x -= 1
-    }
+// const img = new Image();
+// img.src = 'img/DVD_video_logo.png'
 
-    if (x+rectWidth >= canvas.width || x <= 0) {
-        goRight = !goRight
-    }
+// let x = 20
+// let y = 20
+// let moveX = 2
+// let moveY = 2
 
-    if (y+rectHeight >= canvas.height || y <= 0) {
-        goUp = !goUp
+const CUBE_SIZE_X = 100
+const CUBE_SIZE_Y = 100
+
+const objs = [];
+
+const colors = ['#ffba08','#faa307','#f48c06','#e85d04','#dc2f02','#d00000','#9d0208','#6a040f','#370617','#03071e']
+
+let gyroscope = new Gyroscope({frequency: 60});
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+
+
+function init() {
+    let r = canvas.width/2 - 5;
+    for (let i = 0; i < colors.length; i++) {
+        objs.push(crircleCreate(canvas.width/2, canvas.height/2,r,colors[i]));
+        r = r/1.3;
     }
 
 }
 
+function gameloop(){
+
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    init();
+    objs.forEach(obj => drawCircle(obj));
+
+
+    //ctx.drawImage(img, x, y, CUBE_SIZE_X, CUBE_SIZE_Y)
+
+    // if (x+CUBE_SIZE_X >= canvas.width || x <= 0) {
+    //     moveX *= -1
+    // }
+
+    // if (y+CUBE_SIZE_Y >= canvas.height || y <= 0) {
+    //     moveY *= -1
+    // }
+
+    // x+=moveX
+    // y+=moveY
+
+}
+
 setInterval(gameloop, 1000 / 60)
+
+
+function crircleCreate(x,y,r,color) {
+    let obj = {
+        x: x,
+        y: y,
+        r: r,
+        color: color,
+        draw: drawCircle
+    }
+    return obj
+}
+
+function drawCircle(obj){
+    ctx.beginPath();
+    ctx.fillStyle = obj.color;
+    ctx.arc(obj.x, obj.y, obj.r,0, 2 * Math.PI);
+    ctx.fill();
+    ctx.closePath();
+}
